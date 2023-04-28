8db58e99cc1bac5b605bba539abcd01a62c4aea7
Akhil1857 <akhil.trivedi@knoldus.com>
2023-04-28 15:31:44 +0530

    Higher Order Function

--- src/main/scala/HOFTest.scala
+++ src/main/scala/HOFTest.scala
@@ -1,13 +1,15 @@
 object HOFTest extends App{
-  def product(f: Int, number: Int): Int = {
-    increment(5) * number
+  private def product(f:Int=>Int, number: Int): Int = {
+    val resultIncrement = f(5)
+    resultIncrement * number
 
   }
 
-  def increment(digit: Int): Int = {
+  private def increment(digit: Int): Int = {
     digit + 1
   }
 
-  product(5)
+ private val result =  product(increment,5)
+  println(result)
 
 }
